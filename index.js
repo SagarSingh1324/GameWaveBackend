68da5ce8304bfe70d8f352d6d24afcf56fa0f31a
SagarSingh1324 <sagarsinghprj@gmail.com>
2025-07-08 00:41:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+import connectDB from './config/db.js';
+
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+const port = process.env.PORT || 5000;
+
+try {
+    connectDB();
+} catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // Exit the process with failure
+}
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
+app.get('/', (req, res)=>{
+    res.send('api is running');
+});
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,49 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from './asyncHandler.js';
+import User from '../models/userModel.js';
+
+// Protect routes
+const protect = asyncHandler( async (req, res, next ) => {
+    let token;
+
+    // Check if cookies are present and read the JWT from the cookie
+    if(req.cookies && req.cookies.jwt){
+        token = req.cookies.jwt;
+    }
+
+    if(token) {
+        try{
+            // verify the token
+            const decoded = jwt.verify( token, process.env.JWT_SECRET);
+            // fetch the user from database
+            req.user = await User.findById(decoded.userId).select('-password');
+
+            if (!req.user) {
+                res.status(401);
+                throw new Error('Not authorized, user not found');
+            }
+            // proceed to next middleware
+            next();
+        } catch (error){
+            console.error(error);
+            res.status(401);
+            throw new Error('Not authorized, token failed:' + error.message);
+        }
+    } else {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+});
+
+
+// Admin middleware
+const admin = (req, res, next)  => {
+    if(req.user && req.user.isAdmin) {
+        next();
+    } else {
+        req.status(401);
+        throw new Error('Not authorised as an Admin');
+    }
+};
+
+export { protect, admin };
