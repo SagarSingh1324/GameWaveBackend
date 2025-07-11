719f688282258d0eae8a11148e2524d09ab4a6d2
SagarSingh1324 <sagarsinghprj@gmail.com>
2025-07-11 20:45:26 +0530

    added keyword search and pagination in productsController

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,27 +1,73 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public
+// @desc    Fetch all products with optional filtering, search, and pagination
+// @route   GET /api/products?brand=Razer&brand=Logitech&category=Mouse&keyword=Aula&pageNumber=2
+// @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.status(200).json(products);
+    const pageSize = Number(process.env.PAGINATION_LIMIT) || 10;
+    const page = Number(req.query.pageNumber) || 1;
+
+    let { brand, category, keyword } = req.query;
+
+    const filter = {};
+
+    if (keyword) {
+        filter.name = { $regex: keyword, $options: 'i' }; // case-insensitive
+    }
+
+    if (brand) {
+        if (!Array.isArray(brand)) {
+        brand = [brand];
+        }
+        filter.brand = { $in: brand };
+    }
+
+    if (category) {
+        if (!Array.isArray(category)) {
+        category = [category];
+        }
+        filter.category = { $in: category };
+    }
+
+    const count = await Product.countDocuments(filter);
+
+    const products = await Product.find(filter)
+        .limit(pageSize)
+        .skip(pageSize * (page - 1));
+
+    res.json({
+        products,
+        page,
+        pages: Math.ceil(count / pageSize),
+    });
 });
 
-// @desc Fetch a products
+// @desc Fetch a single product by ID
 // @route GET /api/products/:id
 // @access Public
 const getProductById = asyncHandler(async (req, res) => {
-    const product = await Product.findById( req.params.id);
+    const product = await Product.findById(req.params.id);
 
-    if(product){
+    if (product) {
         return res.status(200).json(product);
-    }
-    else{
+    } else {
         res.status(404);
         throw new Error('Resource not found');
     }
 });
 
-export {getProducts, getProductById};
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(6);
+
+  res.json(products);
+});
+
+export { 
+    getProducts, 
+    getProductById,
+    getTopProducts,
+ };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
 const router = express.Router();
-import {getProducts, getProductById} from '../controllers/productController.js';
+import {
+    getProducts, 
+    getProductById, 
+    getTopProducts, 
+} from '../controllers/productController.js';
 
 router.route('/').get(getProducts);
+router.get('/top', getTopProducts);
 router.route('/:id').get(getProductById);
 
 export default router;
